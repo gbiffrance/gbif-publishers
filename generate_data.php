50d52d900d4c487b4a9b6bf2bd7b10897a2cdf25
Gallien LABEYRIE <labeyrie@gbif.fr>
2014-11-06 11:15:54 +0100

    cleaning repo

--- generate_data.php
+++ generate_data.php
@@ -1,6 +1,6 @@
 <?php 
-include_once("config.php");
-include_once("stats.php");
+include_once("config.inc");
+include_once("stats.inc");
 
 
 // Generate a JSON file containing all publishers stats 
